a17cdef55d858ec3da04d3b67907a1c6eeafa89a
wook3024@gmail.com <wook3024@gmail.com>
2025-03-03 15:16:57 +0900

    Enable HTTPS and enhance security configurations
    Switch the server to HTTPS with SSL setup and update the security configuration to include login, logout, and remember-me functionality. Added stricter content security policies, HSTS, and enforced secure connections. Updated Zipkin to use disk storage with a bound data volume.

--- src/main/kotlin/org/example/learnspring/config/SecurityConfig.kt
+++ src/main/kotlin/org/example/learnspring/config/SecurityConfig.kt
@@ -17,10 +17,58 @@ class SecurityConfig {
                     .requestMatchers(
                         "/swagger-ui/**",   // Swagger UI 관련 허용
                         "/v3/api-docs/**",  // OpenAPI 문서 관련 허용
-                        "/swagger-resources/**"  // Swagger 리소스 허용
+                        "/swagger-resources/**",  // Swagger 리소스 허용
+                        "/api/**"  // Swagger 리소스 허용
                     ).permitAll() // 누구나 접근 가능하도록 설정
                     .anyRequest().authenticated() // 나머지 요청은 인증 필요
             }
+            .formLogin {
+                formLogin ->
+                    formLogin
+                        .loginPage("/login")
+                        .permitAll()
+            }
+            .logout { logout ->
+                logout
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/")
+                    .permitAll()
+            }
+            .rememberMe { rememberMe ->
+                rememberMe
+                    .key("uniqueAndSecretKey")
+                    .tokenValiditySeconds(60 * 60 * 24 * 7)
+            }
+            .headers { headers ->
+                headers
+                    .defaultsDisabled()
+                    .contentSecurityPolicy { csp ->
+                        csp.policyDirectives(
+                            "default-src 'self'; " +
+                            "script-src 'self' 'nonce-randomValue'; " +
+                            "style-src 'self' 'nonce-randomValue'; " +
+                            "img-src 'self' data:; " +
+                            "connect-src 'self'; " +
+                            "frame-ancestors 'self'; " +
+                            "form-action 'self'; " +
+                            "base-uri 'self';"
+                        )
+                    }
+                    .httpStrictTransportSecurity { hsts ->
+                        hsts
+                            .includeSubDomains(true)
+                            .maxAgeInSeconds(31536000)
+                    }
+                    .frameOptions{ frameOptions ->
+                        frameOptions
+                            .sameOrigin()
+                    }
+            }
+            .requiresChannel { requiresChannel ->
+                requiresChannel
+                    .anyRequest().requiresSecure()
+            }
+
         return http.build()
     }
 }
